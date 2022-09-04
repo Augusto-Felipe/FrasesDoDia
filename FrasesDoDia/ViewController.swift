aa32d695ffa931dad7b577ccc6425d6962df2b24
Felipe Augusto <augusto_felipe@icloud.com>
2022-09-04 12:10:11 -0300

    First commit

--- FrasesDoDia/ViewController.swift
+++ FrasesDoDia/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaFrase: UILabel!
+    
+    var fraseArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func botaoNovaFrase(_ sender: Any) {
+        fraseArray.append("As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.")
+        fraseArray.append("A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.")
+        fraseArray.append("No meio da dificuldade encontra-se a oportunidade.")
+        
+        legendaFrase.text = fraseArray[Int(arc4random_uniform(3))]
+    }
+    
 }
 
